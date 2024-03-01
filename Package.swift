d46881698ebac02b3097e8ad76650394e516bcf8
Alejandro Gelos <alejandrog@ext.inditex.com>
2024-03-01 11:38:48 +0100

    Create project

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ZipVideos",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "ZipVideos",
+            targets: ["ZipVideos"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "ZipVideos"),
+        .testTarget(
+            name: "ZipVideosTests",
+            dependencies: ["ZipVideos"]),
+    ]
+)
